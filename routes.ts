667eb1e774ace7a87b9313df494e78e823a3640b
Mariusz Łazarz <mariusz.lazarz97@gmail.com>
2024-07-01 16:03:32 +0200

    adding logo to mobile nav

--- routes.ts
+++ routes.ts
@@ -2,12 +2,11 @@ export const publicRoutes = [
   "/",
   "/test",
   "/products",
-  "/admin",
   "/not-found",
   "/api/products",
   "/api/stripe_webhook",
 ];
 
-export const adminRoutes = [];
+export const adminRoutes = ["/admin"];
 
 export const apiAuth = "/api/auth";
