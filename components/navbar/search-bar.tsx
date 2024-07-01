7a3a4b928c2cbeefa5cf5c63d0613a62b58ea399
Mariusz Łazarz <mariusz.lazarz97@gmail.com>
2024-07-02 00:16:05 +0200

    fixing suggestion menu display

--- components/navbar/search-bar.tsx
+++ components/navbar/search-bar.tsx
@@ -72,7 +72,7 @@ const SearchBar = () => {
       />
       <MagnifyingGlassIcon className="h-6 w-6 absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 cursor-pointer" />
       {suggestions.length > 0 && (
-        <ul className="absolute -left-20 md:-left-60 bg-white mt-2 z-10 w-screen md:w-[500px] md:border h-[350px] overflow-auto">
+        <ul className="absolute -left-12 md:-left-60 bg-white mt-2 z-10 w-screen md:w-[500px] md:border h-[350px] overflow-auto">
           {suggestions.map((suggestion) => (
             <li
               key={suggestion.id}
